fe92a1abc0067fd55d5ba7443ee3a8506395457f
SantiagoCastano03 <santiagocasmdp@gmail.com>
2024-11-16 19:23:20 -0300

    comienzo

--- src/compilador/generadorCodigo/GeneracionAssembler.java
+++ src/compilador/generadorCodigo/GeneracionAssembler.java
@@ -5,11 +5,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Stack;
+import compilador.lexer.TablaSimbolos;
 
 public class GeneracionAssembler {
     private static ArrayList<String> polaca;
     private BufferedWriter writer;
-    private static Stack<String> pilaAux = new Stack<>();
+    private static Stack<String> pila = new Stack<>();
+    private int contadorAux = 0;
 
     public GeneracionAssembler(ArrayList<String> representacionPolaca, String rutaArchivo) throws IOException {
         this.polaca = representacionPolaca;
@@ -18,9 +20,44 @@ public class GeneracionAssembler {
 
     public void generarCodigoAssembler() throws IOException{
         generarHeader();
+        for (String token: polaca){
+            procesarToken(token);
+        }
     }
 
-    public void generarHeader() throws IOException{
+    private void procesarToken(String token) throws  IOException{
+
+        switch(token){
+            case "+":
+                String op2 = pila.pop();
+                String op1 = pila.pop();
+                operacionSuma(op1,op2);
+                break;
+            case "-":
+
+        }
+    }
+
+
+    private String getTipo(String op1, String op2, String operador){
+
+    }
+
+    private void operacionSuma(String op1, String op2) throws IOException{
+        String aux = crearVariableAux();
+        writer.write("MOV EAX, _" + op1 + "\n");
+        writer.write("ADD EAX, _" + op2 + "\n");
+        writer.write("MOV " + aux + ", EAX\n");
+        pila.push(aux);
+    }
+
+    private String crearVariableAux() throws IOException {
+        String varAux = "@aux" + (++contadorAux);
+        TablaSimbolos.agregarLexema(varAux);//Agregar auxiliar a la tabla de simbolos
+        return varAux;
+    }
+
+    private void generarHeader() throws IOException{
         writer.write(".386\n");
         writer.write(".model flat, stdcall\n");
         writer.write("option casemap :none\n\n");
