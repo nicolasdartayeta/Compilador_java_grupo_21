6f68585a0ba35d1f682eef1c7bb81529dd5da820
SantiagoCastano03 <santiagocasmdp@gmail.com>
2024-11-16 16:25:22 -0300

    x

--- src/compilador/generadorCodigo/GeneracionAssembler.java
+++ src/compilador/generadorCodigo/GeneracionAssembler.java
@@ -1,18 +1,35 @@
 package compilador.generadorCodigo;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Stack;
 
 public class GeneracionAssembler {
     private static ArrayList<String> polaca;
-    private static StringBuilder assemblerGenerado = new StringBuilder();
+    private BufferedWriter writer;
     private static Stack<String> pilaAux = new Stack<>();
 
-    public GeneracionAssembler(ArrayList<String> representacionPolaca) {
+    public GeneracionAssembler(ArrayList<String> representacionPolaca, String rutaArchivo) throws IOException {
         this.polaca = representacionPolaca;
+        this.writer = new BufferedWriter(new FileWriter(rutaArchivo));
     }
 
+    public void generarCodigoAssembler() throws IOException{
+        generarHeader();
+    }
 
-
-
+    public void generarHeader() throws IOException{
+        writer.write(".386\n");
+        writer.write(".model flat, stdcall\n");
+        writer.write("option casemap :none\n\n");
+        writer.write("include \\masm32\\include\\windows.inc\n");
+        writer.write("include \\masm32\\include\\kernel32.inc\n");
+        writer.write("includelib \\masm32\\lib\\kernel32.lib\n\n");
+        writer.write(".data\n");
+        writer.write("    resultado DWORD ?\n");
+        writer.write(".code\n");
+        writer.write("start:\n");
+    }
 }
