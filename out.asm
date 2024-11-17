5492a4ea09e7d80dbf0581d2605b14ab8bd940db
SantiagoCastano03 <santiagocasmdp@gmail.com>
2024-11-17 13:31:49 -0300

    .

---
+++ out.asm
@@ -0,0 +1,12 @@
+.386
+.model flat, stdcall
+option casemap :none
+
+include \masm32\include\windows.inc
+include \masm32\include\kernel32.inc
+includelib \masm32\lib\kernel32.lib
+
+.data
+	a:main dd 0
+.code
+start:
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import compilador.lexer.Lexer;
 import compilador.lexer.TablaSimbolos;
 import compilador.parser.Parser;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        Parser.main(args);
     }
 }
