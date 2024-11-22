838df7f521980af3dfba903377561e2fd3717d34
Nicolas <nicodarta1305@gmail.com>
2024-11-22 13:17:12 -0300

    candidato de entrega

--- build.ps1
+++ build.ps1
@@ -1,11 +1,34 @@
+# Get the relative path to the source assembly file
+param (
+    [string]$sourceFile
+)
+
+# Check if the source file exists
+if (-Not (Test-Path $sourceFile)) {
+    Write-Host "The specified source file does not exist: $sourceFile"
+    exit
+}
+
+# Get the directory where the source file is located
+$sourceDir = [System.IO.Path]::GetDirectoryName($sourceFile)
+
 # Assemble the source code using MASM
-& "\masm32\bin\ml" /c /Zd /coff out.asm
+& "\masm32\bin\ml" /c /Zd /coff $sourceFile
+
+# Get the base name of the file (without extension)
+$baseName = [System.IO.Path]::GetFileNameWithoutExtension($sourceFile)
 
 # Link the object file to create the executable
-& "\masm32\bin\Link" /SUBSYSTEM:CONSOLE out.obj
+& "\masm32\bin\Link" /SUBSYSTEM:CONSOLE "$baseName.obj"
+
+# Move the generated files (.obj and .exe) to the source directory
+Move-Item "$baseName.obj" $sourceDir
+Move-Item "$baseName.exe" $sourceDir
 
 Write-Host "Ejecucion del programa:"
-& ".\out"
+
+# Execute the generated executable from the source directory
+& "$sourceDir\$baseName.exe"
 
 # Pause for user input before closing
 Read-Host "Press Enter to exit..."
